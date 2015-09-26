c4991f47c02d80393cec5d641a6c4875e74bbb78
Gleb <git@gleb.nl>
2015-09-26 21:31:33 +0200

    Added onclick functionality to the skip video button in video fragment.

--- app/src/main/java/nl/utrecht/uni/questions/questions/Video.java
+++ app/src/main/java/nl/utrecht/uni/questions/questions/Video.java
@@ -1,7 +1,8 @@
 package nl.utrecht.uni.questions.questions;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,7 +37,7 @@ public class Video extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_intro, container, false);
+        return inflater.inflate(R.layout.fragment_video, container, false);
     }
 
     @Override
@@ -51,7 +52,12 @@ public class Video extends Fragment {
         getActivity().findViewById(R.id.btn_skip_video).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // Create new fragment and transaction
+                Fragment newFragment = new Intro();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.container, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
     }
