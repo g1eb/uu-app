5edb57f4dad5b1a5c831ba67b4237fe259082bbc
Gleb <git@gleb.nl>
2015-09-27 01:55:54 +0200

    Added initial question fragment.

---
+++ app/src/main/java/nl/utrecht/uni/questions/questions/Question.java
@@ -0,0 +1,52 @@
+package nl.utrecht.uni.questions.questions;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Question.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Question#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Question extends Fragment {
+
+
+    public static Question newInstance() {
+        Question fragment = new Question();
+
+        return fragment;
+    }
+
+    public Question() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_question, container, false);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
