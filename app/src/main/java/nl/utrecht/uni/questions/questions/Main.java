80d119176d859551e54c32a49d576f79a820337e
Gleb <git@gleb.nl>
2015-09-27 00:01:10 +0200

    Renamed main activity.

--- app/src/main/java/nl/utrecht/uni/questions/questions/Main.java
+++ app/src/main/java/nl/utrecht/uni/questions/questions/Main.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
