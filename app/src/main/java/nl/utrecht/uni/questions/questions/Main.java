1fdaced98ffe1752d576b8dbd444dc1517efcbeb
Gleb <git@gleb.nl>
2015-10-01 10:28:24 +0200

    Disable screen orientation rotation on tilt of the device.

--- app/src/main/java/nl/utrecht/uni/questions/questions/Main.java
+++ app/src/main/java/nl/utrecht/uni/questions/questions/Main.java
@@ -2,6 +2,7 @@ package nl.utrecht.uni.questions.questions;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -44,6 +45,9 @@ public class Main extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOptions);
         getSupportActionBar().hide();
+
+        // Enforce landscape layout
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
     }
 
     public void logQuestion(String question) {
